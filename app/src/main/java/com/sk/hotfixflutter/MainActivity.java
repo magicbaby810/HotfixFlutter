5cbe8f36c3ce20e0c4d45fd7e3c1915d3fa332cd
sk <magicbaby810@gmail.com>
2020-07-01 13:57:03 +0800

    修改 readme

---
+++ app/src/main/java/com/sk/hotfixflutter/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sk.hotfixflutter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView gogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 必须给存储权限啊，要不然tinker的补丁无法写入本地文件夹
+        ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1001);
+
+        gogo = findViewById(R.id.gogo);
+        gogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(FlutterTestActivity.makeIntent(MainActivity.this, null), 1000);
+            }
+        });
+    }
+}
---
+++ flutterhotfixmodule/README.md
@@ -0,0 +1,8 @@
+# flutterhotfixmodule
+
+A new Flutter module.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.dev/).
